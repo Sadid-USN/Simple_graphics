cfbc81f32736bdcce89fb11fdb63cf106f2178d6
Sadid <ulamuyaman@gmail.com>
2021-05-10 19:09:25 +0300

    simple graphics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'chapter_lists.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,36 +7,148 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.blueGrey,
-          textTheme: GoogleFonts.ptSerifTextTheme(Theme.of(context).textTheme),
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+        textTheme: GoogleFonts.ptSerifTextTheme(Theme.of(context).textTheme),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'About pepperoni',
+            style: TextStyle(fontSize: 30),
+          ),
+          centerTitle: true,
         ),
-        debugShowCheckedModeBanner: false,
-        home: GroupListViewPage());
+        drawer: Drawer(),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(bottom: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.blue[400],
+                    border: Border.all(width: 2)),
+                alignment: Alignment.center,
+                height: 45,
+                child: Text(
+                  'Ingredients of italian pepperoni',
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                width: double.infinity,
+                child: Text(
+                  "Pepperoni is made from a mixture of ground beef mixed with spices and"
+                  "flavorings. Salt and sodium nitrate are then added as curing agents, which prevent the"
+                  "growth of unwanted microorganisms. Nitrate is also added, which gives pepperoni its color.",
+                  style: TextStyle(
+                    height: 1.5,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  border: Border.all(width: 2),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow[800],
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow[800],
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow[800],
+                            ),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                          ],
+                        ),
+                        Text(
+                          ' 17 Reviews',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.restaurant,
+                                color: Colors.green,
+                                size: 40,
+                              ),
+                              Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text('Feed')),
+                              Text('2 - 4'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.category,
+                                color: Colors.green,
+                                size: 40,
+                              ),
+                              Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text('Feed')),
+                              Text('2 - 4'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.emoji_food_beverage,
+                                color: Colors.green,
+                                size: 40,
+                              ),
+                              Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text('Feed')),
+                              Text('2 - 4'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
-// SafeArea(
-//         child: Container(
-//           child: Center(
-//             child: Text(
-//             '200\$',
-//             style: TextStyle(
-//             fontWeight: FontWeight.bold,
-//             fontSize: 24,
-//             letterSpacing: 5.0,
-//             wordSpacing: 5.0,
-//             decoration: TextDecoration.lineThrough
-//             ),
-//             ),
-//           ),
-//           decoration: BoxDecoration(
-//             image: DecorationImage(
-//               image: AssetImage('assets/images/mindnode.jpg'),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
